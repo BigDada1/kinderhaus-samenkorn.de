102deab70fbb31986143e929e6f0dfdb6ef7ce8a
BigDada1 <48822301+BigDada1@users.noreply.github.com>
2023-09-28 22:50:02 +0200

    basic-page content implemented

--- site/templates/basic-page.php
+++ site/templates/basic-page.php
@@ -1,5 +1,5 @@
 <?php
 include("./includes/head.inc");
 include("./includes/header.inc");
-// include("./includes/basic-content.inc");
+include("./includes/basic-content.inc");
 include("./includes/foot.inc");
