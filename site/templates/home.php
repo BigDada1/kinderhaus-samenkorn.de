8cc9c79e937b60dd30b64db1d725ef443c9e935e
Clemens Henker <48822301+BigDada1@users.noreply.github.com>
2023-09-30 00:06:49 +0200

    Home template implemented

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,5 +1,9 @@
 <?php
 include("./includes/head.inc");
 include("./includes/header.inc");
+include("./includes/slider.inc");
+include("./includes/simple-body.inc");
+include("./includes/shortcuts.inc");
+include("./includes/article-list.inc");
 // include("./includes/basic-content.inc");
 include("./includes/foot.inc");
