7d6cc54d58e73a62c213f3aa609c3558722f1f86
Clemens Henker <48822301+BigDada1@users.noreply.github.com>
2023-10-04 17:58:01 +0200

    Events-table removed (preview only)

--- site/templates/home.php
+++ site/templates/home.php
@@ -6,5 +6,4 @@ include("./includes/simple-body.inc");
 include("./includes/shortcuts.inc");
 include("./includes/article-list.inc");
 include("./includes/events.inc");
-// include("./includes/events-table.inc");
 include("./includes/foot.inc");
